5fd7feb9267dccf05890d0ae9b4c6322947e19d8
Hebert <hebertdev82@gmail.com>
2026-03-02 16:38:11 -0300

    feat: user terms

--- app/termos/page.jsx
+++ app/termos/page.jsx
@@ -7,7 +7,7 @@ export default function Termos() {
             Termos de Uso do <span className="text-[#1a574f] font-bold">Zapfaztudo</span>.
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam cum deserunt excepturi, officiis iste eveniet inventore numquam neque quos eum repellendus aspernatur laborum voluptatem! Maxime quae illo molestiae eligendi mollitia.
+            Aqui vão os termos de uso.
           </p>
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
